76cc0e858a203dcdb102ed4ff6bd130b9e943354
lokman1313 <lokmanhossenjoy13@gmail.com>
2026-05-09 01:34:19 +0600

    rsponcive website dome

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -42,7 +42,7 @@ const Home = () => {
       </div>
       <div className="my-7">
       <h2 className="text-2xl font-bold py-2">Your Friends</h2>
-     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 ">
+     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-3 ">
       {frinds.map(frind => (
         <Card key={frind.id} frind={frind}></Card>
       ))}
